3ecb251c0aa768a86e83d364e9b74882d2ae5c06
Serhii <99speedy@mail.ru>
2019-12-18 19:18:14 +0200

    Add files via upload

---
+++ homework8/ES 5 Prototype/init.js
@@ -0,0 +1,21 @@
+const aList = new SuperAList();
+
+const lList = new SuperLList();
+
+
+aList.push(1)
+aList.push(2)
+aList.push(3)
+aList.push(4)
+
+
+
+let test = new SuperAList();
+
+test.collection = [{ passport: '03005988', name: 'Joseph Francis Tribbiani Jr', age: 32, sex: 'm' },
+    { passport: '03005989', name: 'Chandler Muriel Bing', age: 33, sex: 'm' },
+    { passport: '03005990', name: 'Ross Eustace Geller', age: 33, sex: 'm' },
+    { passport: '03005991', name: 'Rachel Karen Green', age: 31, sex: 'f' },
+    { passport: '03005992', name: 'Monica Geller', age: 31, sex: 'f' },
+    { passport: '03005993', name: 'Phoebe Buffay', age: 34, sex: 'f' }]
+
